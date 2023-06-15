9a64bb9e9fb05582f5059bdd56713e8553bf79e3
kamal patel <kamal.patel30@gmail.com>
2023-06-15 09:44:50 -0400

    SQL

---
+++ SQL Data Science Specialization/SQL for Data Science/practice.sql
@@ -0,0 +1,69 @@
+1. 
+Table: Products
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| product_id  | int     |
+| low_fats    | enum    |
+| recyclable  | enum    |
++-------------+---------+
+product_id is the primary key for this table.
+low_fats is an ENUM of type ('Y', 'N') where 'Y' means this product is low fat and 'N' means it is not.
+recyclable is an ENUM of types ('Y', 'N') where 'Y' means this product is recyclable and 'N' means it is not.
+
+Write an SQL query to find the ids of products that are both low fat and recyclable.
+
+Return the result table in any order.
+
+-- SQL Code
+SELECT product_id FROM Products
+WHERE low_fats = 'Y' AND recyclable = 'Y'
+
+2.
+Table: Tweets
+
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| tweet_id       | int     |
+| content        | varchar |
++----------------+---------+
+tweet_id is the primary key for this table.
+This table contains all the tweets in a social media app.
+
+Write an SQL query to find the IDs of the invalid tweets. The tweet is invalid if the number of characters used in the content of the tweet is strictly greater than 15.
+
+-- SQL CODE 
+SELECT tweet_id FROM Tweets
+WHERE LENGTH(content) > 15
+
+
+3. 
+Table: World
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| name        | varchar |
+| continent   | varchar |
+| area        | int     |
+| population  | int     |
+| gdp         | bigint  |
++-------------+---------+
+name is the primary key column for this table.
+Each row of this table gives information about the name of a country, the continent to which it belongs, its area, the population, and its GDP value.
+ 
+
+A country is big if:
+
+it has an area of at least three million (i.e., 3000000 km2), or
+it has a population of at least twenty-five million (i.e., 25000000).
+Write an SQL query to report the name, population, and area of the big countries.
+
+Return the result table in any order.
+
+-- SQL Code
+SELECT name, population, area
+FROM World
+WHERE area >= 3000000 OR population >= 25000000;
